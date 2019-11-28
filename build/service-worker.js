e99bcdc8a1efb942e847fda437efa1f36ac60593
余方杨 <a975057282@gmail.com>
2019-11-28 15:52:55 +0800

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d17d2cacb732bfd9804479bfca0714c3.js"
+  "/precache-manifest.69277f1181f958b73dff34cb40a42519.js"
 );
 
 self.addEventListener('message', (event) => {
